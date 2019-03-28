ee80609fa6d5f351da2a7c1c34ac82fd1c4edfac
qiezi0301 <qiezi0301@163.com>
2019-03-29 06:59:42 +0800

    add highlight

--- resources/js/components/lessons/LessonBody.vue
+++ resources/js/components/lessons/LessonBody.vue
@@ -32,9 +32,27 @@
     import MarkdownBody from './../common/markdown-body'
     import 'social-share.js/dist/css/share.min.css';
     import 'social-share.js/dist/js/social-share.min';
+
+    import hljs from 'highlight.js'
+    import 'highlight.js/styles/atom-one-dark.css'
+
+    const highlightCode = () => {
+        const preEl = document.querySelectorAll('pre');
+
+        preEl.forEach((el) => {
+            hljs.highlightBlock(el)
+        })
+    };
+
     export default {
         name: "LessonBody",
         props: ['lesson'],
+        mounted(){
+            highlightCode()
+        },
+        updated () {
+            highlightCode()
+        },
         components:{
             CommentList,
             CommentForm,
